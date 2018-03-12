e54bcc3f47b00d3ca7ca6ad233c9bb6ea3e4ade3
jwwallin <jussi.w.wallin@gmail.com>
2018-03-13 00:59:32 +0200

    add attribution for millionthvector and remove some comments

---
+++ web/intro/assets/graphics/README.md
@@ -0,0 +1,1 @@
+tribase-u3-d0.png is created by [MillionthVector](http://millionthvector.blogspot.fi/ "MillionthVector").
--- web/intro/intro.js
+++ web/intro/intro.js
@@ -6,12 +6,8 @@ function main() {
 
     WebFontConfig = {
 
-      //  'active' means all requested fonts have finished loading
-      //  We set a 1 second delay before calling 'createText'.
-      //  For some reason if we don't the browser cannot render the text the first time it's created.
       active: function () { game.time.events.add(Phaser.Timer.SECOND, timeEvents, this); },
 
-      //  The Google Fonts we want to load (specify as many as you like in the array)
       google: {
         families: ['Arvo']
       }
